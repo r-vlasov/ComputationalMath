7091eec441d1c41d8da2be3a3f976d5f90908397
q4four <vlasov.ri@phystech.edu>
2019-10-28 17:36:48 +0300

    system

---
+++ NewtonMethod/system.py
@@ -0,0 +1,57 @@
+import numpy as np
+from math import *
+
+def Jacoby(x, y):
+	matr = np.array([[2 * x, 2 * y], [-1 / (cos(x) * cos(x)), 1]])
+	return np.linalg.inv(matr)
+
+def Free_coef(x, y):
+	return np.array([x ** 2 + y ** 2 - 1, y - tan(x)])
+
+def JF(x, y):
+	jacoby = Jacoby(x, y)
+	free = Free_coef(x, y)
+	return np.dot(jacoby, free)
+
+eps = 0.00001
+
+def solver(x, y):
+	x0 = np.array([0, 0])
+	x1 = np.array([x, y])
+	while (np.linalg.norm(x0-x1) > eps):
+		x0 = x1
+		x1 = x0 - JF(x0[0], x0[1])
+	return x1
+
+rootlist = list()
+
+step = 0.1
+bot = 0.1
+top = 1
+current_x = bot
+
+def addinlist(rlist, vect):
+	for element in rlist:
+		if abs(element[0] - vect[0]) < eps and abs(element[1] == vect[1]) < eps:
+			return None
+		else:
+			continue
+	rlist.append(vect)
+
+
+
+while (current_x <= top):
+	root = solver(current_x, 0)
+	addinlist(rootlist, root)
+	current_x += step
+
+i = 1
+stop = len(rootlist)
+print(stop)
+for el in rootlist:
+	if i == stop:
+		print(rootlist)
+		exit()
+	if el[0] == rootlist[i][0] and el[1] == rootlist[i][1]:
+		rootlist.remove(el)
+	i += 1
